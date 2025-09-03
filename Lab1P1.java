1ace506c9f27079032d9656ef3bcaeb00ccc4f1b
sriharshini78 <gudipellysriharshini@gmail.com>
2025-09-03 11:29:50 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,89 @@
+class Lab1P1{
+   public static void main(String...args){
+   
+   //int x = 200;
+   //byte b = x;
+   System.out.println("....byte...");	
+   byte b = 15;
+   Byte bb = b;
+   System.out.println(Byte.MAX_VALUE);
+   System.out.println(bb.MIN_VALUE);
+   System.out.println(bb.SIZE);
+   System.out.println(bb.TYPE);
+   System.out.println(bb.BYTES);
+   
+   System.out.println("....short...");
+   short s = 15;
+   Short ss = s;
+   System.out.println(Short.MAX_VALUE);
+   System.out.println(ss.MIN_VALUE);
+   System.out.println(ss.SIZE);
+   System.out.println(ss.TYPE);
+   System.out.println(ss.BYTES);
+   
+   
+   System.out.println("....integers...");
+   int i = 15;
+   Integer ii = i;
+   System.out.println(Integer.MAX_VALUE);
+   System.out.println(ii.MIN_VALUE);
+   System.out.println(ii.SIZE);
+   System.out.println(ii.TYPE);
+   System.out.println(ii.BYTES);
+   
+   System.out.println("....long....");
+   long l = 15;
+   Long ll = l;
+   System.out.println(Long.MAX_VALUE);
+   System.out.println(ll.MIN_VALUE);
+   System.out.println(ll.SIZE);
+   System.out.println(ll.TYPE);
+   System.out.println(ll.BYTES);
+   
+   
+   
+   System.out.println("....float....");
+   float f = 15;
+   Float ff = f;
+   System.out.println(Float.MAX_VALUE);
+   System.out.println(ff.MIN_VALUE);
+   System.out.println(ff.SIZE);
+   System.out.println(ff.TYPE);
+   System.out.println(ff.BYTES);
+   
+   System.out.println("....double....");
+   double d = 15;
+  Double dd= d;
+   System.out.println(Double.MAX_VALUE);
+   System.out.println(dd.MIN_VALUE);
+   System.out.println(dd.SIZE);
+   System.out.println(dd.TYPE);
+   System.out.println(dd.BYTES);
+   
+   System.out.println("....boolean....");
+   boolean z =true;
+   Boolean zz = z;
+   //System.out.println(Boolean.MAX_VALUE);
+   //System.out.println(zz.MIN_VALUE);
+  // System.out.println(zz.SIZE);
+   System.out.println(zz.TYPE);
+  // System.out.println(zz.BYTES);
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   }	
+
+}
