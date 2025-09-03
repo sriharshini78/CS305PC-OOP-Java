dd89779ed9d848a585c32bd2fd6e56e93bd0454a
sriharshini78 <gudipellysriharshini@gmail.com>
2025-09-03 12:39:11 +0530

    Add files via upload

---
+++ LAB1/Lab1P4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Lab1P4{
+	public static void main(String...args){
+	Scanner s = new Scanner(System.in);
+	try{
+	int a = s.nextInt();
+	int b = s.nextInt();
+	
+	System.out.println(a/b);
+	}catch(Exception e){
+	System.out.println(e.getClass());
+	
+	
+	}
+	
+	}
+}
