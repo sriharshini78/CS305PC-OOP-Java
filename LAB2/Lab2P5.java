88bffda6a75ba6b20fd58d29ebbd08364d9b3518
sriharshini78 <gudipellysriharshini@gmail.com>
2025-09-10 12:39:32 +0530

    Add files via upload

---
+++ LAB2/Lab2P5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Lab2P5{
+	public static void main(String...args){
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	int count = 0;
+	int i = 2;
+	while(i<a){
+	if( a % i == 0){
+	System.out.println(" it is  n0t a prime number");
+	}
+	else{
+	System.out.println(" it is a prime number");
+	}
+	break;
+	
+
+		}
+	}
+}
