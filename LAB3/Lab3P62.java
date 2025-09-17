043a3045269ffc4f99d6ef07dec3ab4f6c95affe
sriharshini78 <gudipellysriharshini@gmail.com>
2025-09-17 12:49:28 +0530

    Add files via upload

---
+++ LAB3/Lab3P62.java
@@ -0,0 +1,12 @@
+class Lab3P62 extends Lab3P61{
+	@Override
+	public  void demonstrate(){
+		System.out.println(" I AM A METHOD IN LAB3P62");
+	}
+	Lab3P62(){
+		super.demonstrate();
+	}
+	
+	
+	
+}
