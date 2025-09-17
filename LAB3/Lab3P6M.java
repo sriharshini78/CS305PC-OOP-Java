2e07e9ec28c8e24a5e9ace3ffb79e3252cd730a1
sriharshini78 <gudipellysriharshini@gmail.com>
2025-09-17 12:49:52 +0530

    Add files via upload

---
+++ LAB3/Lab3P6M.java
@@ -0,0 +1,9 @@
+class Lab3P6M{
+	public static void main(String...args){
+		Lab3P62 p = new Lab3P62();
+		
+		p.setName("ACE ENGINEERING COLLEGE");
+		System.out.println(p.getName());
+		p.demonstrate();
+	}
+}
