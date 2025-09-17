3899fc3494e68d47a52413bca93be6135d68d6b9
sriharshini78 <gudipellysriharshini@gmail.com>
2025-09-17 12:48:53 +0530

    Add files via upload

---
+++ LAB3/Lab3P61.java
@@ -0,0 +1,14 @@
+class Lab3P61{
+	 String name;
+	public  void demonstrate(){
+		System.out.println(" I AM A METHOD IN LAB3P61");
+	}
+	public void setName(String s){
+	 	this.name=s;
+	}
+	public String getName(){
+	 	return this.name;
+	}	
+
+
+}
