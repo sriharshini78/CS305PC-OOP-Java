88586f8443e9e0b829c01cbb786b13ee8f68d39e
sriharshini78 <gudipellysriharshini@gmail.com>
2025-09-24 12:41:22 +0530

    Add files via upload

---
+++ PROJECT/StudentMain.java
@@ -0,0 +1,11 @@
+package Student;
+import java.util.Scanner;
+import java.io.*;
+class StudentMain implements Operations{
+	public static void main(String...args){
+	 	static String name , id , branch , password;
+	 	Scanner sc = new Scanner(System.in);
+
+
+	}
+}
