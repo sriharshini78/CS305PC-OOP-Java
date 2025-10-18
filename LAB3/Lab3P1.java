dab4a3313f5c56f96acbebd2fbcd84074eea8056
sriharshini78 <gudipellysriharshini@gmail.com>
2025-10-18 10:28:37 +0530

    Refactor prime number detection logic

--- LAB3/Lab3P1.java
+++ LAB3/Lab3P1.java
@@ -1,32 +1,23 @@
 import java.util.Scanner;
-class Lab3P1{
-	public static void main(String...args){
-	Scanner s = new Scanner(System.in);
-	int a = s.nextInt();
-	int i = 2;
-	while(i<a){
-		int j =2,count =0;
-		if(i == 2){
-			count++;
-		}
-		while(j<i){
-		
-		if(i % j == 0){
-		count =0;
-		break;
-		}
-		else{
-		
-			count++;
-			j++;
-		}
-		
-		}
-		if(count > 0){
-		System.out.println(i);
-		
-	       }
-	       i++;
-	       }
-	}
+
+class Lab3P1 {
+    public static void main(String... args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+
+        for (int i = 2; i < a; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
 }
