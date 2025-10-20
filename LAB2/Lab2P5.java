945ce7d15d8fdb7f47ac9459342f8b063f5bd55c
sriharshini78 <gudipellysriharshini@gmail.com>
2025-10-20 17:21:54 +0530

    Update Lab2P5.java

--- LAB2/Lab2P5.java
+++ LAB2/Lab2P5.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
-
-class Lab2P5 {
-    public static void main(String... args) {
+class Lab2p5{
+    public static void main(String...args){
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+
         int a = s.nextInt();
         int count = 0;
-        int i = 2;
 
-        while (i <= a / 2) {
-            if (a % i == 0) {
+        int i = 2;
+        while(i < a){
+            if(a % i == 0){
                 count++;
                 break;
             }
             i++;
         }
 
-        if (count == 0 && a > 1) {
-            System.out.println("It is a prime number");
-        } else {
-            System.out.println("It is not a prime number");
-        }
+        if(count == 0)
+            System.out.println("Prime number");
+        else
+            System.out.println("Number is not prime");
     }
 }
